27d6bbd0ad0874df876b3b7c6c66008940252c8a
AarnavSawant <aarnav.sawant@gmail.com>
2020-05-17 23:23:11 -0700

    Initial Commit

---
+++ covidAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covidAI
+//
+//  Created by Parikshat Sawant on 5/17/20.
+//  Copyright © 2020 Sawant,Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
